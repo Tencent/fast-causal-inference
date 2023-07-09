0896959dff06ee9563383fa21b305b4b7c2ce1e4
huangyanyanyan <749843959@qq.com>
2023-07-09 13:19:21 +0800

    image update

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e
+set -eu
+set -o pipefail
+
+history_path=`pwd -L`
+base_path=$(dirname $(readlink -e $0))
+
 history_path=`pwd`
 base_path=$(cd $(dirname $0)/..; pwd)
 cd $base_path
@@ -7,7 +12,7 @@ if [ ! -f "$base_path/contrib/ClickHouse/LICENSE" ];then
   echo "fetch contrib submodule"
   git submodule update --init --recursive
 fi
-cp $base_path/src/udf/ClickHouse/src/AggregateFunctions/* $base_path/contrib/ClickHouse/src/AggregateFunctions/
+cp -f $base_path/src/udf/ClickHouse/src/AggregateFunctions/* $base_path/contrib/ClickHouse/src/AggregateFunctions/
 cd $base_path/contrib/ClickHouse/; mkdir -p build
 cmake -S . -B build
 cd build; ninja clickhouse
