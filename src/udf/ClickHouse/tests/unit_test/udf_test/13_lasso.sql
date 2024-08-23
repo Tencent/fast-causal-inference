0bd6497021cc110474eb005a7bef5076508f1c23
FFFFFFFHHHHHHH <916677625@qq.com>
2024-08-23 18:54:54 +0800

    add clickhouse udf

--- src/udf/ClickHouse/tests/unit_test/udf_test/13_lasso.sql
+++
@@ -1,7 +0,0 @@
-WITH (
-        SELECT stochasticLogisticRegressionState(0.1, 0.1, 1., 'Lasso')(Y, X1, X2, X3)
-        FROM causal_inference_test
-    ) AS model
-SELECT round(sum(evalMLMethod(model, X1, X2, X3))/1000)
-FROM causal_inference_test;
-
--- src/udf/ClickHouse/tests/unit_test/udf_test/9_group_set.sql
+++
@@ -1,7 +0,0 @@
-SELECT arrayJoin(GroupSet(X1, treatment, X2, X3)) AS a
-FROM causal_inference_test
-ORDER BY a.6 ASC;
-
-SELECT arrayJoin('Column1', 'Column2')(GroupSet(X1, treatment, X2, X3)) AS a
-FROM causal_inference_test
-ORDER BY a.6 ASC;
